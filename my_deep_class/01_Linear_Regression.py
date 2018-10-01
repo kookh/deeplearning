fd3a2cc1582b7ace3369ef3e5660165a33dcd0a4
koo\kookh <kookh0987@gmail.com>
2018-10-01 11:29:54 +0900

    선형회귀 : 최소제곱법

--- my_deep_class/01_Linear_Regression.py
+++ my_deep_class/01_Linear_Regression.py
@@ -1,3 +1,6 @@
+# 선형 회귀 : 방정식
+# 기울기 : 최소제곱법
+
 import numpy as np
 
 x = [2, 4, 6, 8]
@@ -5,9 +8,30 @@ y = [81, 93, 91, 97]
 
 mx = np.mean(x)
 my = np.mean(y)
-
+print("x의 평균값:", mx)
+print("y의 평균값:", my)
 # print(mx, my)
 
+# for i in x:
+#     print(i)
+
+# divisor : 분모
+divisor = sum([(mx - i)**2 for i in x])
+print("분모:", divisor)
+
+# dividend : 분자
+def top(x, mx, y, my):
+    d = 0
+    for i in range(len(x)):
+        d += (x[i] - mx) * (y[i] - my)
+    return d
+
+dividend = top(x, mx, y, my)
+print("분자:", dividend)
 
 
+a = dividend / divisor
+b = my - (mx*a)
 
+print("기울기 a =", a)
+print("y 절편 b =", b)
