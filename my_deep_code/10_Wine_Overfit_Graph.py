9849779671f9344e1e57987603dca3c6aa428843
koo\kookh <kookh0987@gmail.com>
2018-10-04 11:28:47 +0900

    # 와인의 종류 예측하기 : 학습의 자동중단

--- my_deep_code/10_Wine_Overfit_Graph.py
+++ my_deep_code/10_Wine_Overfit_Graph.py
@@ -2,12 +2,10 @@
 
 from keras.models import Sequential
 from keras.layers.core import Dense
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import EarlyStopping
 
 import pandas as pd
 import numpy
-import os
-import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
@@ -32,30 +30,8 @@ model.add(Dense(1, activation='sigmoid'))
 # 모델 컴파일
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 모델 저장 폴더 설정
-MODEL_DIR = './model/'
-if not os.path.exists(MODEL_DIR):
-    os.mkdir(MODEL_DIR)
+early_stopping_callback = EarlyStopping(monitor='val_loss', patience=100)
 
-modelpath="./model/{epoch:02d}-{val_loss:.4f}.hdf5"
-checkpointer = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1, save_best_only=True)
+model.fit(X, Y, validation_split=0.2, epochs=2000, batch_size=500, callbacks=[early_stopping_callback])
 
-# 모델 실행 및 저장
-history = model.fit(X, Y, validation_split=0.33, epochs=3500, batch_size=500)
-
-# y_vloss에 테스트셋으로 실험 결과의 오차 값을 저장
-y_vloss=history.history['val_loss']
-
-# y_acc에 학습셋으로 측정한 정확도의 값을 저장
-y_acc=history.history['acc']
-
-# x 값을 지정하고 정확도를 파란색으로, 오차를 빨간색으로 표시
-x_len = numpy.arange(len(y_acc))
-plt.plot(x_len, y_vloss, "o", c="red", markersize=3)
-plt.plot(x_len, y_acc, "o", c="blue", markersize=3)
-
-plt.show()
-#
-# model.fit(X, Y, validation_split=0.2, epochs=200, batch_size=200, verbose=0, callbacks=[checkpointer])
-
-# print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
+print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
