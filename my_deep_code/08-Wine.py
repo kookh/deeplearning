bb14dc500ef07d8f01120019a4e86de46b2a05e8
koo\kookh <kookh0987@gmail.com>
2018-10-04 11:01:02 +0900

    # 와인의 종류 예측하기 : 모델 업데이트

--- my_deep_code/08-Wine.py
+++ my_deep_code/08-Wine.py
@@ -1,13 +1,14 @@
-# 와인의 종류 예측하기 : 데이터 확인과 실행
+# 와인의 종류 예측하기 : 모델 업데이트
 
 from keras.models import Sequential
 from keras.layers.core import Dense
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.callbacks import ModelCheckpoint
 
 import pandas as pd
 import numpy
+import os
 import tensorflow as tf
-import matplotlib.pyplot as plt
+
 
 seed = 0
 numpy.random.seed(seed)
@@ -29,7 +30,15 @@ model.add(Dense(1, activation='sigmoid'))
 # 모델 컴파일
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 모델 실행
-model.fit(X, Y, epochs=200, batch_size=200)
+# 모델 저장 폴더 설정
+MODEL_DIR = './model/'
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
+
+modelpath="./model/{epoch:02d}-{val_loss:.4f}.hdf5"
+checkpointer = ModelCheckpoint(filepath=modelpath, monitor='val_loss', verbose=1)
+
+# 모델 실행 및 저장
+model.fit(X, Y, validation_split=0.2, epochs=200, batch_size=200, verbose=0, callbacks=[checkpointer])
 
 print("\n Accuracy: %.4f" % (model.evaluate(X, Y)[1]))
